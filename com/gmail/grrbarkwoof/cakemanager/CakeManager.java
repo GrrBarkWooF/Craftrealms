b40dcdb252f61be7c55f7f1016b9c552e34b3b5f
Vincen Gutiérrez <kkonato@gmail.com>
2014-10-31 16:37:31 -0600

    Let's get this started

---
+++ com/gmail/grrbarkwoof/cakemanager/CakeManager.java
@@ -0,0 +1,23 @@
+package com.gmail.grrbarkwoof.cakemanager;
+
+import org.bukkit.plugin.java.*;
+import org.bukkit.command.*;
+import org.bukkit.plugin.*;
+import org.bukkit.*;
+
+public final class CakeManager extends JavaPlugin
+{ @Override
+  public void onEnable()
+  {
+  }
+
+  @Override
+  public void onDisable()
+  {
+  }
+
+  @Override
+  public boolean onCommand(CommandSender sender, Command command, String alias, String[] args)
+  { return false;
+  }
+}
